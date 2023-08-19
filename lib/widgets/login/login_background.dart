e03f5a4f174ab01e48b5badf96eff389bbd17caf
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-08-19 00:10:30 -0600

    Add bubbles to background

--- lib/widgets/login/login_background.dart
+++ lib/widgets/login/login_background.dart
@@ -26,11 +26,62 @@ class _PurpleBox extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: screenSize.height * 0.4,
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromRGBO(63, 63, 156, 1),
-        Color.fromRGBO(90, 70, 178, 1)
-      ])),
+      decoration: _backgroundDecoration(),
+      child: const Stack(
+        children: [
+          Positioned(
+            top: 90,
+            left: 30,
+            child: _Buble(),
+          ),
+          Positioned(
+            top: -40,
+            left: -30,
+            child: _Buble(),
+          ),
+          Positioned(
+            top: -50,
+            right: -20,
+            child: _Buble(),
+          ),
+          Positioned(
+            bottom: -50,
+            left: 10,
+            child: _Buble(),
+          ),
+          Positioned(
+            bottom: 120,
+            right: 20,
+            child: _Buble(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  BoxDecoration _backgroundDecoration() {
+    return const BoxDecoration(
+        gradient: LinearGradient(colors: [
+      Color.fromRGBO(63, 63, 156, 1),
+      Color.fromRGBO(90, 70, 178, 1)
+    ]));
+  }
+}
+
+class _Buble extends StatelessWidget {
+  const _Buble({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: const Color.fromRGBO(255, 255, 255, 0.05),
+      ),
     );
   }
 }
