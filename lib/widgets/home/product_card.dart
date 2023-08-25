6fc4eba8481faed62bde6c4e69eb6bc5657cfe31
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-08-24 18:25:51 -0600

    style card

--- lib/widgets/home/product_card.dart
+++ lib/widgets/home/product_card.dart
@@ -6,10 +6,30 @@ class ProductCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: _cardDecoration(),
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
       width: double.infinity,
       height: 400,
-      color: Colors.red,
     );
+  }
+
+  BoxDecoration _cardDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 7),
+              blurRadius: 10,
+            )
+          ]);
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
