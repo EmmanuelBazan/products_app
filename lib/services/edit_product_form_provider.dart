b55b00cd52f8ab952b8c0e89cd7e47f2f16f1888
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-08-28 12:03:37 -0600

    Edit product provider implementation

---
+++ lib/services/edit_product_form_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:products_app/models/product.dart';
+
+class EditProductFormProvider extends ChangeNotifier {
+  GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+  Product product;
+
+  EditProductFormProvider(this.product);
+
+  bool isValidForm() {
+    return formKey.currentState?.validate() ?? false;
+  }
+}
